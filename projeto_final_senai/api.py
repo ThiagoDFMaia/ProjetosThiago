e7a777eee60f4881227f96b5e4e25fe8865aed6d
thiago <thi.henriquemaia@gmail.com>
2024-11-18 13:01:59 -0300

    finalizando

--- projeto_final_senai/api.py
+++ projeto_final_senai/api.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import StandardScaler
 
 import numpy as np
 from biblioteca import *
+import pickle
 
 
 
@@ -22,77 +23,29 @@ app= FastAPI()
 auth2_scheme=OAuth2PasswordBearer(tokenUrl='token')
 
 # rodar api: uvicorn api:app --reload 
-
+import os
 async def carrega_modelo_cancer():
-    try:
-        print (50*"#","carregando dataset cancer")
-        df = pd.read_csv('dataset/cancer_mama.csv')
-        print(df.head())
-        print(df.info())
-        df = df.drop(columns = "id")
-        df['diagnosis'] = df['diagnosis'].map({'M': 1, 'B': 0})
-        corr=df.corr()
-        print(corr['diagnosis'])
-        classe = df['diagnosis']
-        df = df.drop(columns = 'diagnosis')
-        df=df.drop(columns='Unnamed: 32')
-        x_test, x_train, y_test, y_train = train_test_split(df, classe, test_size=0.3, random_state=42)
-        modelo=MLPClassifier(hidden_layer_sizes=(25,20,15), activation='logistic',alpha=0.1 ,max_iter=15500)
+    print("carregando modelo cancer")
+    if os.path.exists("modelocancer.pkl"):
+        print(f"Tamanho do arquivo: {os.path.getsize('modelocancer.pkl')} bytes")
+    if os.path.getsize("modelocancer.pkl") == 0:
+        print("Erro: O arquivo está vazio!")
+    
 
-
-       
-        modelo.fit(x_train, y_train)
-        acuracia_treino=modelo.score(x_train, y_train)
-        print(f"Acurácia de treino modelo de cancer: {(acuracia_treino*100):.2f}%")
-        y_pred =  modelo.predict(x_test)
-
-
-        acuracia_teste = accuracy_score(y_test, y_pred)
-        print(f"Acurácia de teste do modelo de cancer: {(acuracia_teste*100):.2f}%")
-
-       
-       
-    except FileNotFoundError:
-        print("Erro: O arquivo de dataset de cancer não foi encontrado.")
-        return None
-    except Exception as e:
-        print(f"Ocorreu um erro ao carregar o dataset de cancer: {e}")
-        return None
-    return modelo
+    with open('modelocancer.pkl', 'rb') as arquivo:
+        return pickle.load(arquivo)
 
 async def carrega_modelo_infarto():
-    try:
-        print (50*"#","carregando dataset infarto")
-        df = pd.read_csv('dataset/heart.csv')
-        print(df.head())
-        print(df.info())
-        corr=df.corr()
-        print(corr['output'])
+    print("carregando modelo infarto")
+    if os.path.exists("modeloinfarto.pkl"):
+        print(f"Tamanho do arquivo: {os.path.getsize('modeloinfarto.pkl')} bytes")
+    if os.path.getsize("modeloinfarto.pkl") == 0:
+        print("Erro: O arquivo está vazio!")
+    
 
+    with open('modeloinfarto.pkl', 'rb') as arquivo:
+        return pickle.load(arquivo)
 
-        classe = df['output']
-        df.drop('output',axis=1,inplace=True)
-        X_train,X_test,Y_train,Y_test=train_test_split(df,classe,test_size=0.3,random_state=42)
-        modelo=MLPClassifier(activation='logistic',  hidden_layer_sizes=(10, 10), max_iter=2000, random_state=42)
-
-      
-
-        modelo.fit(X_train, Y_train)
-        acuracia_treino=modelo.score(X_train, Y_train)
-        print(f"Acurácia de treino modelo de infarto: {(acuracia_treino*100):.2f}%")
-
-
-        y_pred =  modelo.predict(X_test)
-        acuracia_teste = accuracy_score(Y_test, y_pred)
-        print(f"Acurácia de teste do modelo de infarto: {(acuracia_teste*100):.2f}%")
-     
-    except FileNotFoundError:
-        print("Erro: O arquivo de dataset de infarto não foi encontrado.")
-        return None
-    except Exception as e:
-        print(f"Ocorreu um erro ao carregar o dataset de infarto: {e}")
-        return None
-    return modelo
 
 @app.on_event("startup")
 async def load_dataset():
@@ -106,108 +59,156 @@ async def load_dataset():
 async def index():
     return {"mensagem": "API está funcionando e os datasets foram carregadod!"}
 
+class EntradaModeloCancer(BaseModel):
+    radius_mean: float
+    texture_mean: float
+    perimeter_mean: float
+    area_mean: float
+    smoothness_mean: float
+    compactness_mean: float
+    concavity_mean: float
+    concave_points_mean: float
+    symmetry_mean: float
+    fractal_dimension_mean: float
+    radius_se: float
+    texture_se: float
+    perimeter_se: float
+    area_se: float
+    smoothness_se: float
+    compactness_se: float
+    concavity_se: float
+    concave_points_se: float
+    symmetry_se: float
+    fractal_dimension_se: float
+    radius_worst: float
+    texture_worst: float
+    perimeter_worst: float
+    area_worst: float
+    smoothness_worst: float
+    compactness_worst: float
+    concavity_worst: float
+    concave_points_worst: float
+    symmetry_worst: float
+    fractal_dimension_worst: float
+
 @app.post('/prever_cancer/')
-async def prever_cancer(features: CancerFeatures):
-
-    colunas_treinamento = [
-    'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean',
-    'compactness_mean', 'concavity_mean', 'concave points_mean', 'symmetry_mean', 'fractal_dimension_mean',
-    'radius_se', 'texture_se', 'perimeter_se', 'area_se', 'smoothness_se', 'compactness_se', 'concavity_se',
-    'concave points_se', 'symmetry_se', 'fractal_dimension_se', 'radius_worst', 'texture_worst', 'perimeter_worst',
-    'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave points_worst', 'symmetry_worst',
-    'fractal_dimension_worst'
-]
-
-# Criando um DataFrame com colunas do treinamento
-    dados = pd.DataFrame([[
-    features.radius_mean,
-    features.texture_mean,
-    features.perimeter_mean,
-    features.area_mean,
-    features.smoothness_mean,
-    features.compactness_mean,
-    features.concavity_mean,
-    features.concave_points_mean,  
-    features.symmetry_mean,
-    features.fractal_dimension_mean,
-    features.radius_se,
-    features.texture_se,
-    features.perimeter_se,
-    features.area_se,
-    features.smoothness_se,
-    features.compactness_se,
-    features.concavity_se,
-    features.concave_points_se,  
-    features.symmetry_se,
-    features.fractal_dimension_se,
-    features.radius_worst,
-    features.texture_worst,
-    features.perimeter_worst,
-    features.area_worst,
-    features.smoothness_worst,
-    features.compactness_worst,
-    features.concavity_worst,
-    features.concave_points_worst,  
-    features.symmetry_worst,
-    features.fractal_dimension_worst
-]], columns=colunas_treinamento)
-
-    # Realiza a previsão usando o modelo
+async def prever_cancer(dados: EntradaModeloCancer):
+    print('prevendo')
     try:
-     
-
-        # Agora, passe o DataFrame para o modelo
-        probabilidades = modelo_cancer.predict_proba(dados)
-
-        confianca = max(probabilidades[0]) * 100  # Converte para porcentagem
-
-        resposta = modelo_cancer.predict(dados)  
-        print(f'previsao: {resposta} confiança: {confianca:.2f}%')
-        return {"previsao": int(resposta), "confianca":confianca}
-    except Exception as e:
-      
-        raise HTTPException(status_code=500, detail=f"Erro na previsão: {e}")
-
-
-@app.post('/prever_infarto/')
-async def prever_infarto(features: InfartoFeatures):
+        # Converter os dados em um array numpy
+        entrada = np.array([[
+            dados.radius_mean,
+            dados.texture_mean,
+            dados.perimeter_mean,
+            dados.area_mean,
+            dados.smoothness_mean,
+            dados.compactness_mean,
+            dados.concavity_mean,
+            dados.concave_points_mean,
+            dados.symmetry_mean,
+            dados.fractal_dimension_mean,
+            dados.radius_se,
+            dados.texture_se,
+            dados.perimeter_se,
+            dados.area_se,
+            dados.smoothness_se,
+            dados.compactness_se,
+            dados.concavity_se,
+            dados.concave_points_se,
+            dados.symmetry_se,
+            dados.fractal_dimension_se,
+            dados.radius_worst,
+            dados.texture_worst,
+            dados.perimeter_worst,
+            dados.area_worst,
+            dados.smoothness_worst,
+            dados.compactness_worst,
+            dados.concavity_worst,
+            dados.concave_points_worst,
+            dados.symmetry_worst,
+            dados.fractal_dimension_worst
+        ]])  # Envolvendo em uma lista para o modelo aceitar como input
+        # Fazer a previsão
     
-    colunas_treinamento = [
-    'age', 'sex', 'cp', 'trtbps', 'chol', 'fbs', 'restecg', 'thalachh', 'exng', 'oldpeak', 
-    'slp', 'caa', 'thall'
-]
 
-    # Criando um DataFrame com as colunas do treinamento para infarto
-    dados = pd.DataFrame([[
-    features.age,
-    features.sex,
-    features.cp,
-    features.trtbps,
-    features.chol,
-    features.fbs,
-    features.restecg,
-    features.thalachh,
-    features.exng,
-    features.oldpeak,
-    features.slp,
-    features.caa,
-    features.thall
-]], columns=colunas_treinamento)
 
-    # Realiza a previsão usando o modelo
-    try:
-     
-
-        # Agora, passe o DataFrame para o modelo
-        probabilidades = modelo_infarto.predict_proba(dados)
-
-        confianca = max(probabilidades[0]) * 100  # Converte para porcentagem
-
-        resposta = modelo_infarto.predict(dados)  
-        print(f'previsao: {resposta} confiança: {confianca:.2f}%')
-        return {"previsao": int(resposta), "confianca":confianca}
+        
+        previsao = modelo_cancer.predict(entrada)
+       
+        probabilidades = modelo_cancer.predict_proba(entrada)
+        classe_predita = previsao[0]  # Pega a classe predita
+        confianca = probabilidades[0][classe_predita]  # Pega a probabilidade da classe predita
+        print(f'Resposta: {previsao[0]} confiança: {confianca:.2f}%')
+        return {"previsao": int(previsao[0]),"confianca":confianca}
     except Exception as e:
-      
-        raise HTTPException(status_code=500, detail=f"Erro na previsão: {e}")
+        print(f'erro: {e}')
+        
+'''
+const data = {
+        age: age,
+        sex: sex,
+        cp: cp,
+        trtbps: trtbps,
+        chol: chol,
+        fbs: fbs,
+        restecg: restecg,
+        thalachh: thalachh,
+        exng: exng,
+        oldpeak: oldpeak,
+        slp: slp,
+        caa: caa,
+        thall: thall
+    };
+'''
+
+class EntradaModeloInfarto(BaseModel):
+        age: int
+        sex: int
+        cp: int
+        trtbps: int
+        chol: int
+        fbs: int
+        restecg: int
+        thalachh: int
+        exng: int
+        oldpeak: float
+        slp: int
+        caa: int
+        thall: int
+@app.post('/prever_infarto/')
+async def prever_infarto(dados: EntradaModeloInfarto):
+    
+    print('prevendo')
+    try:
+        # Converter os dados em um array numpy
+        entrada = np.array([[
+           dados.age,
+           dados.sex,
+           dados.cp,
+           dados.trtbps,
+           dados.chol,
+           dados.fbs,
+           dados.restecg,
+           dados.thalachh,
+           dados.exng,
+           dados.oldpeak,
+           dados.slp,
+           dados.caa,
+           dados.thall
+        ]])  # Envolvendo em uma lista para o modelo aceitar como input
+        # Fazer a previsão
+    
+
+
+        
+        previsao = modelo_infarto.predict(entrada)
+       
+        probabilidades = modelo_infarto.predict_proba(entrada)
+        classe_predita = previsao[0]  # Pega a classe predita
+        confianca = probabilidades[0][classe_predita]  # Pega a probabilidade da classe predita
+        print(f'Resposta: {previsao[0]} confiança: {confianca:.2f}%')
+        return {"previsao": int(previsao[0]),"confianca":confianca}
+    except Exception as e:
+        print(f'erro: {e}')
 
 
